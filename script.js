4e255bf3b5b9d4e6984c01fd3f242693782e7a17
Lee117489 <linasmx@gmail.com>
2022-05-11 17:45:08 +0300

    Add grid size change slider and reset button

--- script.js
+++ script.js
@@ -1,28 +1,68 @@
+const DEFAULT_SIZE = 25;
+
+let currentSize = DEFAULT_SIZE;
+
+function setCurentSize(newSize) {
+    currentSize = newSize;
+}
+
+
 const container = document.querySelector('.container');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const reset = document.getElementById('resetGrid');
+
+
+
+
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+reset.onclick = () => resetGrid();
 
 let isClicked = false;
-container.addEventListener('mousedown', () => isClicked = true);
-container.addEventListener('mouseup', () => isClicked = false);
+document.body.addEventListener('mousedown', () => isClicked = true);
+document.body.addEventListener('mouseup', () => isClicked = false);
 
 function createGrid(size) {
     const squareSize = 600 / size;
     for (let i = 0; i < size*size; i++) {
-        const box = document.createElement('div');
-        box.classList.add('pixel');
-        box.style.width = squareSize + 'px';
-        box.style.height = squareSize + 'px';
-        container.appendChild(box);
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.style.width = squareSize + 'px';
+        pixel.style.height = squareSize + 'px';
+        pixel.addEventListener('mouseover', paintBlack);
+        pixel.addEventListener('mousedown', paintBlack);
+        container.appendChild(pixel);
     }
 }
 
-function paintPixels() {
-    const pixels = document.querySelectorAll('.pixel');
-    pixels.forEach(pixel => pixel.addEventListener('mouseover', paintBlack));
+function resetGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(currentSize);
 }
+
+function changeSize(value) {
+    setCurentSize(value);
+    updateSizeValue(value);
+    resetGrid();
+}
+
+function updateSizeValue(value) {
+
+    sizeValue.innerHTML = `${value} x ${value}`;
+}
+
 
 function paintBlack(e) {
-    if (isClicked === true) e.target.classList.add('black');
+    if (e.type === 'mouseover' && !isClicked) return;
+    e.target.classList.add('black');
 }
 
-createGrid(16);
-paintPixels();
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE);
+}
+
+
